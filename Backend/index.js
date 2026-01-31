bc0bec2e9ea38e936eb81c79e274d1577f3d45db
LALIT MAITY <maitylalit89@gmail.com>
2026-01-31 19:32:26 +0530

    Update Again

--- Backend/index.js
+++ Backend/index.js
@@ -6,6 +6,9 @@ app.use(express.json());
 const port=process.env.PORT||4000;
 app.get('/',(req,res)=>{
     res.send("Hello Lalit");
+});
+app.get('/profile',(req,res)=>{
+    res.send("Hello Lalit");
 })
 app.listen(port,()=>{
     console.log(`Server running at ${port}`);
